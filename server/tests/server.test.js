43e7a64411877ddf8eba3ed62ba704607a817218
Daniel <daniel@stoutlabs.com>
2017-11-17 01:50:57 -0500

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,17 @@
 const expect = require("expect");
 const request = require("supertest");
+const { ObjectID } = require("mongodb");
 
 const { app } = require("./../server");
 const { Todo } = require("./../models/todo");
 
 const todos = [
   {
+    _id: new ObjectID(),
     text: "First test todo"
   },
   {
+    _id: new ObjectID(),
     text: "Play some golf"
   }
 ];
@@ -74,6 +77,36 @@ describe("GET /todos", () => {
       .expect(200)
       .expect(res => {
         expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
+});
+
+describe("GET /todos/:id", () => {
+  it("should return a 404 code when using a non-object id", done => {
+    request(app)
+      .get(`/todos/123abc`)
+      .expect(404)
+      .end(done);
+  });
+
+  it("should return a 404 code if todo not found", done => {
+    var id = new ObjectID().toHexString();
+
+    request(app)
+      .get(`/todos/${id}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it("should return todo doc", done => {
+    var id = todos[0]._id.toHexString();
+
+    request(app)
+      .get(`/todos/${id}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe("First test todo");
       })
       .end(done);
   });
