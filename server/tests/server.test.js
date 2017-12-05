0a817d0738c4b3bd5e1445f0ae0394f28c3be7e6
Daniel <daniel@stoutlabs.com>
2017-12-05 02:49:45 -0500

    Add seed data for testing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,27 +4,10 @@ const { ObjectID } = require("mongodb");
 
 const { app } = require("./../server");
 const { Todo } = require("./../models/todo");
+const { todos, users, populateTodos, populateUsers } = require("./seed/seed");
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: "First test todo"
-  },
-  {
-    _id: new ObjectID(),
-    text: "Play some golf",
-    completed: true,
-    completedAt: 420
-  }
-];
-
-beforeEach(done => {
-  Todo.remove({})
-    .then(() => {
-      return Todo.insertMany(todos);
-    })
-    .then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe("POST /todos", () => {
   it("should create a new todo", done => {
